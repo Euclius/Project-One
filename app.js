0077372cdaee456770075992549fb714e7f3065d
Samuel Schaefer <samuelschaefer@Samuels-MacBook-Pro.local>
2019-02-15 14:39:53 -0500

    feeling pretty upset right now. I can get my input field to show my attempted guess(es), but it's only showing submit instead of what I actually typed in. I revisited the to_do and the atm exercise and I cannot figure it out. Too I have made no progress in showing my blank spaceswhen I click begin the game. The code I currently have does nothing to assist that.

--- app.js
+++ app.js
@@ -1,42 +1,44 @@
 $(document).ready(function(){
-console.log('working')
+console.log('js connected')
 
 let hangmanwords = ["chicken", "vampire", "abdomens", "scrutiny", "sphinx", "iceboxing", "tranquilizer"]
-let word = ' '
-let iput = ''
+let word = ''
+let attempts = ''
 
 $('.begin').on('click', getRandomWord = () => {
 let subm = Math.floor(Math.random() * hangmanwords.length)
 word = hangmanwords[subm]
 console.log(word.length)
+// var undsc = ["<div class='underline'</div>"]
+// for (let i = 0; i < word.length;i++) {
+//     $('.underline').append(undsc)
+    // var undsc = word.length
 let undsc = []
 for (let i = 0; i < word.length;i++) {
-    undsc[i] = "_"
+undsc[i] = "_"
 }
-var letters = word.length
 })
+var undsc1 = word.length
+// var undsc = word.length
+// })
+
+// })
 
 $('#field .guessbutn').on('click', function (){
     console.log('jppo')
-    let smit = $('#field .guessbutn').val()
-    iput = smit
-    $('#field .')
-    // if ('.input .guessbutn')
-})
+    const sub = ($('.guessbutn').val())
+    $('#attempts').append(`<li>${sub}</li>`)
+}) //when I get this damn thing to work, I'll try to limit the characters
 // if ('.input' === null) {
 // } else if ('.input'.length !==1) {
-//     shake('.input')
+//     alert("hjk")
+//     // tremble($('.input'))
 // } else {
 
 // }
-
+// })
 
 // const alphabet= ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
 // 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
 // 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-
-
-
-
 })
