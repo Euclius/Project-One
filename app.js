10f083a8df73eb8e543619874467ad6a31497c29
Samuel Schaefer <samuelschaefer@Samuels-MacBook-Pro.local>
2019-02-15 12:24:10 -0500

    i got my submit button to do something in the console log, even though it's an error. But it's connected, so I suppose that's something.

--- app.js
+++ app.js
@@ -3,18 +3,34 @@ console.log('working')
 
 let hangmanwords = ["chicken", "vampire", "abdomens", "scrutiny", "sphinx", "iceboxing", "tranquilizer"]
 let word = ' '
+let iput = ''
 
 $('.begin').on('click', getRandomWord = () => {
 let subm = Math.floor(Math.random() * hangmanwords.length)
 word = hangmanwords[subm]
 console.log(word.length)
-// let undsc = []
-// for (let i = 0; i < word.length;i++) {
-//     undsc[i] = "_"
-// }
+let undsc = []
+for (let i = 0; i < word.length;i++) {
+    undsc[i] = "_"
+}
+var letters = word.length
 })
-$('.guess')
-// var letters = word.length
+
+$('#field .guessbutn').on('click', function (){
+    console.log('jppo')
+    let smit = $('#field .guessbutn').val()
+    iput = smit
+    $('#field .')
+    // if ('.input .guessbutn')
+})
+// if ('.input' === null) {
+// } else if ('.input'.length !==1) {
+//     shake('.input')
+// } else {
+
+// }
+
+
 // const alphabet= ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
 // 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
 // 't', 'u', 'v', 'w', 'x', 'y', 'z']
