33fa8d1e772dd173c4dd3a34534d53e014b76483
Samuel Schaefer <samuelschaefer@Samuels-MacBook-Pro.local>
2019-02-15 15:04:35 -0500

    got the value of what I type into the input field to actually be the correct value instead of saying object object over and over or submit submit. so slightly closer to what I envisioned initially, but still a ways to go.

--- app.js
+++ app.js
@@ -6,16 +6,17 @@ let word = ''
 let attempts = ''
 
 $('.begin').on('click', getRandomWord = () => {
-let subm = Math.floor(Math.random() * hangmanwords.length)
+let subm = Math.floor(Math.random() * hangmanwords.length)      //I don't like that this is random rn, but when I finally compplete the basic logic, I'll make it go in order of the array from simpliest to most difficult
 word = hangmanwords[subm]
 console.log(word.length)
 // var undsc = ["<div class='underline'</div>"]
-// for (let i = 0; i < word.length;i++) {
+// for (let i = 0; i < word.length;i++) {     //looked at Seth's code because he has his underscores performing what I'd like, but it does nothing. Leaving it here for now.
 //     $('.underline').append(undsc)
     // var undsc = word.length
 let undsc = []
 for (let i = 0; i < word.length;i++) {
 undsc[i] = "_"
+$('.keyword').append(undsc)
 }
 })
 var undsc1 = word.length
@@ -23,10 +24,10 @@ var undsc1 = word.length
 // })
 
 // })
-
+// down below is my attempt to move the guesses to the attempted location. it currently fails
 $('#field .guessbutn').on('click', function (){
     console.log('jppo')
-    const sub = ($('.guessbutn').val())
+    const sub = ($('.input').val())
     $('#attempts').append(`<li>${sub}</li>`)
 }) //when I get this damn thing to work, I'll try to limit the characters
 // if ('.input' === null) {
