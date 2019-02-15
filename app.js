b7ec101bddc43e4afab7fb582a48265c57168d3d
Samuel Schaefer <samuelschaefer@Samuels-MacBook-Pro.local>
2019-02-15 11:48:12 -0500

    I've got it in my console log when I click the -begin the game- button, that it is attached randomly to my words in the array. Too I saw something online that will make it to my resources that shows how to implement an input field of a single character.

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 $(document).ready(function(){
 console.log('working')
 
+let hangmanwords = ["chicken", "vampire", "abdomens", "scrutiny", "sphinx", "iceboxing", "tranquilizer"]
+let word = ' '
 
+$('.begin').on('click', getRandomWord = () => {
+let subm = Math.floor(Math.random() * hangmanwords.length)
+word = hangmanwords[subm]
+console.log(word.length)
+// let undsc = []
+// for (let i = 0; i < word.length;i++) {
+//     undsc[i] = "_"
+// }
+})
+$('.guess')
+// var letters = word.length
+// const alphabet= ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+// 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+// 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 
 
