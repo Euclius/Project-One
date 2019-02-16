c1f496b92b8a90f5df401e5eeef90a928b2c60aa
Samuel Schaefer <samuelschaefer@Samuels-MacBook-Pro.local>
2019-02-15 20:25:29 -0500

    after an hour of failing to being able to get my underscores to appear properly, I asked for assistance. Cameron helped me out. My problem with tht was my appending function was within my for looping function but not within my randomWord function. I had tried it being outside of the randomWord function and it did not work right. So one lesson learned. Second thing I spent an hour on and failed to operate correctly was my making my input field only accept a single character. For that I had stopped trying to use val because when I ws doing it it was targetting my submit button which i have as an input with the value of submit. But the primary reason it was not working was that I did not have the dollar sign; I didn't think it was necessary to do within the if function because I ws lready clling a function. And too, I didn't declare function outside of that function to cll into it. nd I was trgetting the input rather than the button for the first bit of it, which should hve been my button clss. and for my if $ I should hve used input rather than input on the entire thing. few lessons lerned in tht mistake. nd t the current moment, my first letter of the lphbet is not working right. I press first letter of english nd it is not inputting. I'm sorry for the difficult reding, I will try to mend it or just work 'round the issue.

--- app.js
+++ app.js
@@ -1,45 +1,60 @@
-$(document).ready(function(){
-console.log('js connected')
+$(document).ready(function () {
+    console.log('js connected')
 
-let hangmanwords = ["chicken", "vampire", "abdomens", "scrutiny", "sphinx", "iceboxing", "tranquilizer"]
-let word = ''
-let attempts = ''
+    let hangmanwords = ["chicken", "vampire", "abdomens", "scrutiny", "sphinx", "iceboxing", "tranquilizer"]
+    let word = ''
 
-$('.begin').on('click', getRandomWord = () => {
-let subm = Math.floor(Math.random() * hangmanwords.length)      //I don't like that this is random rn, but when I finally compplete the basic logic, I'll make it go in order of the array from simpliest to most difficult
-word = hangmanwords[subm]
-console.log(word.length)
-// var undsc = ["<div class='underline'</div>"]
-// for (let i = 0; i < word.length;i++) {     //looked at Seth's code because he has his underscores performing what I'd like, but it does nothing. Leaving it here for now.
-//     $('.underline').append(undsc)
-    // var undsc = word.length
-let undsc = []
-for (let i = 0; i < word.length;i++) {
-undsc[i] = "_"
-$('.keyword').append(undsc)
-}
+    $('.begin').on('click', getRandomWord = () => {
+        let subm = Math.floor(Math.random() * hangmanwords.length)      //I don't like that this is random rn, but when I finally compplete the basic logic, I'll make it go in order of the array from simpliest to most difficult
+        word = hangmanwords[subm]
+        console.log(word.length)
+        console.log(word)
+        // var undsc = ["<div class='underline'</div>"]
+        // for (let i = 0; i < word.length;i++) {     //looked at Seth's code because he has his underscores performing what I'd like, but it does nothing. Leaving it here for now as reference.
+        //     $('.underline').append(undsc)
+        // var undsc = word.length
+        let undsc = []
+        for (let i = 0; i < word.length; i++) {
+            console.log(word[i])
+            undsc[i] = "_" + " "
+            console.log(undsc)
+        }
+        $('.keyword').append(undsc)
+    })
+    var undsc = word.length
+
+    // })
+
+    // })
+    // down below is my attempt to move the guesses to the attempted location. it currently succeeds
+    function attemptedGuess() {
+        const sub = ($('.input').val())
+        $('#attempts').append(`<li>${sub}</li>`)
+        //when I get this damn thing to work, I'll try to limit the characters
+    }
+
+    // if input is not one character, then I want it to decline input
+    //if input is one character, I want it to accept the input
+    // if ('.input'.length === 1) {
+    //     return
+    // } else if ('.input'.length !==1) {
+    //     alert("hjk")
+    //     // shake($('.input'))
+    // } else { ('input'.length === null) 
+    // }
+    $('.guessbutn').on('click', function () {
+        if ($('.input').val().length !== 1) {
+            alert('please enter only one character')
+        } else {
+            attemptedGuess()
+            // const sub = ($('.input').val())
+            // $('#attempts').append(`<li>${sub}</li>`)
+        }
+    })
+
 })
-var undsc1 = word.length
-// var undsc = word.length
-// })
 
-// })
-// down below is my attempt to move the guesses to the attempted location. it currently fails
-$('#field .guessbutn').on('click', function (){
-    console.log('jppo')
-    const sub = ($('.input').val())
-    $('#attempts').append(`<li>${sub}</li>`)
-}) //when I get this damn thing to work, I'll try to limit the characters
-// if ('.input' === null) {
-// } else if ('.input'.length !==1) {
-//     alert("hjk")
-//     // tremble($('.input'))
-// } else {
-
-// }
-// })
 
 // const alphabet= ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
 // 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
 // 't', 'u', 'v', 'w', 'x', 'y', 'z']
-})
